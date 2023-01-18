d7e26217361c1fa5717f61c2d98b7c8684f3a2a2
smhpr <smhpr@183.103.32.109>
2023-01-18 20:03:55 +0900

    implements month maximum

--- src/calender/Calender.java
+++ src/calender/Calender.java
@@ -1,5 +1,7 @@
 package calender;
 
+import java.util.Scanner;
+
 public class Calender {
 
 	public static void main(String[] args) {
@@ -10,7 +12,16 @@ public class Calender {
 		System.out.println("15\t16\t17\t18\t19\t20\t21");
 		System.out.println("22\t23\t24\t25\t26\t27\t28");
 
-
+		//숫자 입력받아 해당하는 달의 최대일수를 출력하는 프로그램
+		Scanner sc=new Scanner(System.in);
+		System.out.print("입력할 달을 선택:");
+		int month=sc.nextInt();
+		
+		int[] monthDays= {31,28,31,30,31,30,31,31,30,31,30,31};
+		
+		System.out.printf("%d 월은 %d 일까지 있습니다.", month,monthDays[month-1]);
+		sc.close();
+		
 	}
 
 }
