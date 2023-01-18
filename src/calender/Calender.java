4b839465fe77bfcfbd1c375053a5627ce28be02c
smhpr <smhpr@183.103.32.109>
2023-01-18 19:33:43 +0900

    Update Calender main()

--- src/calender/Calender.java
+++ src/calender/Calender.java
@@ -4,6 +4,12 @@ public class Calender {
 
 	public static void main(String[] args) {
 		System.out.println("Hello,Calender");
+		System.out.println("----------------------------------------------------");
+		System.out.println(" 1\t 2\t 3\t 4\t 5\t 6\t 7");
+		System.out.println(" 8\t 9\t10\t11\t12\t13\t14");
+		System.out.println("15\t16\t17\t18\t19\t20\t21");
+		System.out.println("22\t23\t24\t25\t26\t27\t28");
+
 
 	}
 
